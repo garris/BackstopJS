16031a4887d2d4eea32f57658cc61a7c9ba9eef1
Chapabu <chapabu101@gmail.com>
2015-02-23 10:56:55 +0000

    Added args array for holding arguments to pass to CasperJS/PhantomJS. Concatinates tests and args array into a casperArgs array to pass to spawn(casperProcess).

--- gulp/tasks/echo.js
+++ gulp/tasks/echo.js
@@ -7,10 +7,14 @@ gulp.task('echo',function(){
 
   var tests = ['capture/echoFiles.js'];
 
+  var args = ['--ssl-protocol=any'];
+
+  var casperArgs = tests.concat(args);
+
   // var args = ['test'].concat(tests); //this is required if using casperjs test option
 
   var casperProcess = (process.platform === "win32" ? "casperjs.cmd" : "casperjs");
-  var casperChild = spawn(casperProcess, tests); //use args here to add test option to casperjs execute stmt
+  var casperChild = spawn(casperProcess, casperArgs); //use args here to add test option to casperjs execute stmt
 
   casperChild.stdout.on('data', function (data) {
     console.log('CasperJS:', data.toString().slice(0, -1)); // Remove \n
