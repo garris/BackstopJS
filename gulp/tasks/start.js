8610f567e78bdc3df43814e6316cc06e738f98a7
Alexander Schneider <alexander.schneider@jankowfsky.com>
2015-05-19 16:13:23 +0200

    Add argument for shutdown time

--- gulp/tasks/start.js
+++ gulp/tasks/start.js
@@ -3,6 +3,7 @@ var fs    = require('fs');
 var exec  = require('child_process').exec;
 var spawn = require('child_process').spawn;
 var paths = require('../util/paths');
+var argv  = require('yargs').argv;
 
 
 
@@ -10,27 +11,34 @@ var paths = require('../util/paths');
 //IF ALREADY STARTED IT WILL NOT TRY TO START AGAIN
 gulp.task("start",function(){
 
-  fs.readFile(paths.serverPidFile, function(err,data){
+  fs.readFile(paths.serverPidFile, function(err,data) {
 
-    if(data){
-      exec('kill -0 '+data,function(error, stdout, stderr){
+    if (data) {
+      exec('kill -0 '+data,function(error, stdout, stderr) {
         if(/no such process/i.test(stderr))
           start();
       });
 
-    }else{
+    } else {
       start();
     }
 
   });
 
+  function start() {
+    var time = (Number(argv.t) === argv.t && argv.t % 1 === 0) ? argv.t : 15;
 
-  function start(){
-    var serverHook = spawn('node', ['server.js'],  {detached: true, stdio:'ignore'});
+    var serverHook = spawn('node', ['server.js', '-t ' + time],  {detached: true, stdio:'ignore'});
     serverHook.unref();
-    fs.writeFileSync(paths.serverPidFile,serverHook.pid);
-    console.log('\nServer launched in background with PID: '+serverHook.pid)
-    console.log('NOTE: Sever will auto-shutdown (default time 15 mins). See documentation for more info.\n')
+    fs.writeFileSync(paths.serverPidFile, serverHook.pid);
+    console.log('\nServer launched in background with PID: '+serverHook.pid);
+
+    if (time > 0) {
+      console.log('NOTE: Sever will auto-shutdown in ' + time + ' mins.\n');
+    } else {
+      console.log('NOTE: Sever will run until you stop it with \'gulp stop\'.\n')
+    }
+
   }
 
 
