670b27825c977834ce6c0c1c6798a5c47b8ca71d
s-d-p <e@thesecretaddress.com>
2015-06-09 17:11:59 -0400

    Missing semi-colins

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -30,8 +30,8 @@ gulp.task('test',['init'], function () {
       var cache = fs.readFileSync(paths.captureConfigFileNameCache, 'utf8');
       if(config !== cache){
         console.log('\nIt looks like the reference configuration has been changed since last reference batch.');
-        console.log('Please run `$ gulp reference` to generate a fresh set of reference files')
-        console.log('or run `$ gulp bless` then `$ gulp test` to enable testing with this configuration.\n\n')
+        console.log('Please run `$ gulp reference` to generate a fresh set of reference files');
+        console.log('or run `$ gulp bless` then `$ gulp test` to enable testing with this configuration.\n\n');
         return;
       }
 
