cd8ea1fdc7003510c4a1b585df29cd10c916df04
Garris Shipon <garris@me.com>
2015-08-04 08:24:55 -0700

    genDefaultCompareConfig now cb-able

--- gulp/util/genDefaultCompareConfig.js
+++ gulp/util/genDefaultCompareConfig.js
@@ -6,9 +6,13 @@ var configDefault = {
   "testPairs": []
 };
 
-var genDefaultCompareConfig = function () {
-  fs.writeFileSync(paths.compareConfigFileName, JSON.stringify(configDefault,null,2));
+var genDefaultCompareConfig = function (cb) {
+  fs.writeFile(paths.compareConfigFileName, JSON.stringify(configDefault,null,2), function(err){
+    if (err) {
+      console.log('error: while genDefaultCompareConfig();')
+    }
+    if (typeof cb == 'object') cb();
+  });
 };
-
 
 module.exports = genDefaultCompareConfig;
