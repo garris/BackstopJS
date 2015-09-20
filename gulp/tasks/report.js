7f3433642739a96adf50d74fb2fc0647170a6438
Garris Shipon <garris@me.com>
2015-09-19 21:34:19 -0700

    cleaned up report switches

--- gulp/tasks/report.js
+++ gulp/tasks/report.js
@@ -1,16 +1,13 @@
 var gulp = require('gulp');
 var paths = require('../util/paths');
 
-
 gulp.task('report',['start'],function(){
 
-  //If not server-only test then run browser report
-  if (paths.report && paths.report.indexOf( 'SSR' ) > -1 ){
-    setTimeout(function(){gulp.run('openReport')},100);
+  if (!paths.report || paths.report.indexOf( 'browser' ) > -1 ){
+    setTimeout(function(){gulp.run('openReport')},300);
   }
 
-  //If not browser-only test then run server side report
-  if (paths.report && paths.report.indexOf( 'browser' ) > -1 ){
-    setTimeout(function(){gulp.run('compare')},300);
+  if (!paths.report || paths.report.indexOf( 'SSR' ) > -1 ){
+    setTimeout(function(){gulp.run('compare')},100);
   }
 });
