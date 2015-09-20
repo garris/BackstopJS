60f4a89ba3822b3fe8a98c619c38a0cb0c4e068e
Garris Shipon <garris@me.com>
2015-09-19 22:01:23 -0700

    hail mary sequence fix

--- gulp/tasks/report.js
+++ gulp/tasks/report.js
@@ -4,10 +4,10 @@ var paths = require('../util/paths');
 gulp.task('report',['start'],function(){
 
   if (!paths.report || paths.report.indexOf( 'browser' ) > -1 ){
-    setTimeout(function(){gulp.run('openReport')},300);
+    setTimeout(function(){gulp.run('openReport')},100);
   }
 
   if (!paths.report || paths.report.indexOf( 'SSR' ) > -1 ){
-    setTimeout(function(){gulp.run('compare')},100);
+    setTimeout(function(){gulp.run('compare')},1000);
   }
 });
