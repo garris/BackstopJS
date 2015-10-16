1662058bf4572c77516d966f493d3339a2016b68
Robert O'Rourke <robert@o-rourke.org>
2015-10-16 14:39:21 +0300

    2 spaces for tabs

--- test/onReadyScript/onReady.js
+++ test/onReadyScript/onReady.js
@@ -1,14 +1,14 @@
 module.exports = function(casper, scenario) {
-	casper.echo('Opening nav menu');
+  casper.echo('Opening nav menu');
 
-	//casper.mouse.move('.navbar-toggle');
-	//casper.click('.navbar-toggle');
+  //casper.mouse.move('.navbar-toggle');
+  //casper.click('.navbar-toggle');
 
-	// PhantomJS fails to load the referenced jquery so using a more crude method to change the DOM
-	casper.evaluate(function(){
-		var navbar = document.getElementById('bs-navbar');
-		navbar.className += ' in';
-	});
+  // PhantomJS fails to load the referenced jquery so using a more crude method to change the DOM
+  casper.evaluate(function(){
+    var navbar = document.getElementById('bs-navbar');
+    navbar.className += ' in';
+  });
 
-	casper.wait(1000);
+  casper.wait(1000);
 };
