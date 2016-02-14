65cc74fb69e5bfd37a9f5a38c824139ce89e2d93
Primajin <github@eyesbound.com>
2016-02-14 13:59:44 +0100

    Implement garris' comments.

--- gulp/util/paths.js
+++ gulp/util/paths.js
@@ -49,6 +49,9 @@ paths.serverPidFile                 = paths.backstop + '/server.pid';
 // ACTIVE CAPTURE CONFIG PATH
 paths.activeCaptureConfigPath       = '';
 
+// DEFAULT FILENAME PATTERN
+paths.fileNameTemplate              = "{configFile}_{scenarioIndex}_{scenarioLabel}_{selectorIndex}_{selectorLabel}_{viewportIndex}_{viewportLabel}";
+
 if(fs.existsSync(paths.backstopConfigFileName)){
   console.log('\nBackstopJS Config loaded at location', paths.backstopConfigFileName);
   paths.activeCaptureConfigPath = paths.backstopConfigFileName;
@@ -68,6 +71,7 @@ if(fs.existsSync(paths.activeCaptureConfigPath)){
     paths.casper_scripts = config.paths.casper_scripts || null;
   }
 
+  paths.fileNameTemplate = config.fileNameTemplate || paths.fileNameTemplate;
   paths.portNumber = config.port || defaultPort;
   paths.casperFlags = config.casperFlags || null;
   paths.engine = config.engine || null;
