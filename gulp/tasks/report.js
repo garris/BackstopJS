5eb17101940b3f927445c5e0e0cd566f2d73e027
Garris Shipon <garris@me.com>
2016-03-10 14:38:59 -0800

    added back openReport delay

--- gulp/tasks/report.js
+++ gulp/tasks/report.js
@@ -4,7 +4,7 @@ var paths = require('../util/paths');
 gulp.task('report',['start'],function(){
 
   if (!paths.report || paths.report.indexOf( 'browser' ) > -1 ){
-    setTimeout(function(){gulp.run('openReport')},100);
+    setTimeout(function(){gulp.run('openReport')},1000);
   }
 
   if (!paths.report || paths.report.indexOf( 'CLI' ) > -1 ){
