690e0dadfe3988febc341d7a63f574ba12df1568
Jannis Hell <jh@mixedzone.io>
2016-06-14 15:54:48 +0200

    Add changes from @Pofigizm

--- gulp/tasks/_init_.js
+++ gulp/tasks/_init_.js
@@ -2,5 +2,7 @@ var paths                   = require('../util/paths');
 var fs                     = require('fs');
 
 var config = require(paths.activeCaptureConfigPath);
+config.configFilePath = paths.activeCaptureConfigPath || '';
+
 // Serialize config as JSON into capture config.
 fs.writeFileSync(paths.captureConfigFileName, JSON.stringify(config));
