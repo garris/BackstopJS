e692d99a4c3caa9ed369886bd96bd307be0c9b6e
Garris Shipon <garris@me.com>
2016-08-24 22:28:52 -0700

    fixed makeConfig file reading issue
    makeConfig no longer attempts to read a config file when executing in the context of genConfig.

--- core/util/makeConfig.js
+++ core/util/makeConfig.js
@@ -3,7 +3,8 @@ var temp = require('temp');
 var fs = require('./fs');
 
 function makeConfig (argv) {
-  var config = {};
+  var config = {},
+      CMD_REQUIRES_CONFIG = !/genConfig/.test(argv['_'][0]);
 
   config.args = argv;
 
@@ -29,12 +30,12 @@ function makeConfig (argv) {
   }
 
   console.log('\nBackstopJS CWD: ', config.customBackstop);
-  console.log('BackstopJS loading config: ', config.backstopConfigFileName, '\n');
 
   // LOAD CONFIG
   var userConfig = {};
-  if (config.backstopConfigFileName && fs.existsSync(config.backstopConfigFileName)) {
+  if (CMD_REQUIRES_CONFIG && config.backstopConfigFileName && fs.existsSync(config.backstopConfigFileName)) {
     try {
+      console.log('BackstopJS loading config: ', config.backstopConfigFileName, '\n');
       userConfig = require(config.backstopConfigFileName);
     } catch (e) {
       console.error('Error ' + e);
@@ -75,7 +76,7 @@ function makeConfig (argv) {
 
   config.casperFlags = userConfig.casperFlags || null;
   config.engine = userConfig.engine || null;
-  config.report = userConfig.report || [ 'CI', 'browser' ];
+  config.report = userConfig.report || [ 'browser' ];
   config.ciReport = userConfig.ci ? {
     format: userConfig.ci.format || config.ci.format,
     testReportFileName: userConfig.ci.testReportFileName || config.ci.testReportFileName,
