ba8b5c13769ceeb48458bc082373f743961ac89c
Uğur Mirza Zeyrek <mirza@artbees.net>
2016-12-19 17:27:53 +0300

    temp folder for hashed captureConfigFile.

--- core/util/extendConfig.js
+++ core/util/extendConfig.js
@@ -1,7 +1,10 @@
 var path = require('path');
 var temp = require('temp');
+var fs = require('fs');
+var hash = require('object-hash');
+const tmpdir = require('os').tmpdir();
 
-function extendConfig(config, userConfig) {
+function extendConfig (config, userConfig) {
   bitmapPaths(config, userConfig);
   ci(config, userConfig);
   htmlReport(config, userConfig);
@@ -18,7 +21,7 @@ function extendConfig(config, userConfig) {
   return config;
 }
 
-function bitmapPaths(config, userConfig) {
+function bitmapPaths (config, userConfig) {
   config.bitmaps_reference = path.join(config.projectPath, 'backstop_data', 'bitmaps_reference');
   config.bitmaps_test = path.join(config.projectPath, 'backstop_data', 'bitmaps_test');
   if (userConfig.paths) {
@@ -27,7 +30,7 @@ function bitmapPaths(config, userConfig) {
   }
 }
 
-function ci(config, userConfig) {
+function ci (config, userConfig) {
   config.ci_report = path.join(config.projectPath, 'backstop_data', 'ci_report');
   if (userConfig.paths) {
     config.ci_report = userConfig.paths.ci_report || config.ci_report;
@@ -47,7 +50,7 @@ function ci(config, userConfig) {
   }
 }
 
-function htmlReport(config, userConfig) {
+function htmlReport (config, userConfig) {
   config.html_report = path.join(config.projectPath, 'backstop_data', 'html_report');
   config.openReport = userConfig.openReport || true;
 
@@ -59,17 +62,25 @@ function htmlReport(config, userConfig) {
   config.compareReportURL = path.join(config.html_report, 'index.html');
 }
 
-function comparePaths(config) {
+function comparePaths (config) {
   config.comparePath = path.join(config.backstop, 'compare');
   config.tempCompareConfigFileName = temp.path({suffix: '.json'});
 }
 
-function captureConfigPaths(config) {
-  config.captureConfigFileName = path.join(config.backstop, 'capture', 'config.json');
+function captureConfigPaths (config) {
+  var captureDir = path.join(tmpdir, 'capture');
+  if (!fs.existsSync(captureDir)) {
+    fs.mkdirSync(captureDir);
+  }
+  var tempCompareConfigFileName = config.tempCompareConfigFileName;
+  config.tempCompareConfigFileName = '';
+  var configHash = hash(config);
+  config.tempCompareConfigFileName = tempCompareConfigFileName;
+  config.captureConfigFileName = path.join(tmpdir, 'capture', configHash + '.json');
   config.captureConfigFileNameDefault = path.join(config.backstop, 'capture', 'config.default.json');
 }
 
-function casper(config, userConfig) {
+function casper (config, userConfig) {
   config.casper_scripts = path.join(config.projectPath, 'backstop_data', 'casper_scripts');
   config.casper_scripts_default = path.join(config.backstop, 'capture', 'casper_scripts');
 
