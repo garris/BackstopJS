255cb7edb28875edb662265959db632090178aea
Garris Shipon <garris@me.com>
2017-02-27 14:36:19 -0800

    using p-map instead of bluebird

--- core/command/approve.js
+++ core/command/approve.js
@@ -1,6 +1,6 @@
 var fs = require('../util/fs');
 var path = require('path');
-var map = require('bluebird').map;
+var map = require('p-map');
 
 var FAILED_DIFF_RE = /^failed_diff/
 
@@ -8,16 +8,17 @@ var FAILED_DIFF_RE = /^failed_diff/
 module.exports = {
   execute: function (config) {
     // TODO:  IF Exists config.bitmaps_test  &&  list.length > 0n  (otherwise throw)
-    console.log('Copying bitmaps from ' + config.bitmaps_test + ' to ' + config.bitmaps_reference);
+    console.log('Copying from ' + config.bitmaps_test + ' to ' + config.bitmaps_reference + '.');
     return fs.readdir(config.bitmaps_test,(err, list) => {
       var src = path.join(config.bitmaps_test, list[list.length-1]);
       return fs.readdir(src, (err, files) => {
+        console.log('The following files will be promoted to reference...');
         return map(files, (file) => {
           if (FAILED_DIFF_RE.test(file)) {
             return true;
           }
-          console.log("Approved > " + file);
-          return fs.copy(path.join(src, file), path.join(config.bitmaps_reference, file))
+          console.log('> ', file);
+          return fs.copy(path.join(src, file), path.join(config.bitmaps_reference, file));
         })
       });
     });
