ef952e628e6c0f5c730d64822d364fbe77e5012f
Garris Shipon <garris@me.com>
2017-06-19 21:25:36 -0700

    integrating chromy wip

--- core/util/createBitmaps.js
+++ core/util/createBitmaps.js
@@ -1,8 +1,10 @@
 var path = require('path');
-
 var fs = require('./fs');
 var each = require('./each');
+var pMap = require('p-map');
+
 var runCasper = require('./runCasper');
+var runChromy = require('./runChromy');
 
 var logger = require('./logger')('createBitmaps');
 
@@ -13,13 +15,7 @@ function regexTest (string, search) {
   return re.test(string);
 }
 
-/**
- * Utility for generating a temporary config file required by GENERATE_BITMAPS_SCRIPT.
- * @config  {Object}        Base user config object (derrived by user config file + CL param overrides).
- * @isReference  {Boolean}  True if running reference flow.
- * @return {Promise}        Resolves when fs.writeFile has completed.
- */
-function writeReferenceCreateConfig (config, isReference) {
+function decorateConfigForCapture (config, isReference) {
   var configJSON;
 
   if (typeof config.args.config === 'object') {
@@ -27,7 +23,15 @@ function writeReferenceCreateConfig (config, isReference) {
   } else {
     configJSON = require(config.backstopConfigFileName);
   }
+  configJSON.scenarios = configJSON.scenarios || [];
 
+  var totalScenarioCount = configJSON.scenarios.length;
+
+  var screenshotNow = new Date();
+  var screenshotDateTime = screenshotNow.getFullYear() + pad(screenshotNow.getMonth() + 1) + pad(screenshotNow.getDate()) + '-' + pad(screenshotNow.getHours()) + pad(screenshotNow.getMinutes()) + pad(screenshotNow.getSeconds());
+
+  configJSON.screenshotDateTime = screenshotDateTime;
+  configJSON.env = config;
   configJSON.isReference = isReference;
   configJSON.paths.tempCompareConfigFileName = config.tempCompareConfigFileName;
   configJSON.defaultMisMatchThreshold = config.defaultMisMatchThreshold;
@@ -36,7 +40,6 @@ function writeReferenceCreateConfig (config, isReference) {
 
   if (config.args.filter) {
     var scenarii = [];
-
     config.args.filter.split(',').forEach(function (filteredTest) {
       each(configJSON.scenarios, function (scenario) {
         if (regexTest(scenario.label, filteredTest)) {
@@ -44,16 +47,82 @@ function writeReferenceCreateConfig (config, isReference) {
         }
       });
     });
-
-    logger.log('Will generate ' + scenarii.length + ' out of ' + configJSON.scenarios.length + ' scenarios');
-
     configJSON.scenarios = scenarii;
   }
 
-  return fs.writeFile(config.captureConfigFileName, JSON.stringify(configJSON));
+  logger.log('Selcted ' + configJSON.scenarios.length + ' of ' + totalScenarioCount + ' scenarios.');
+  return configJSON;
+}
+
+/**
+ * Utility for generating a temporary config file required by GENERATE_BITMAPS_SCRIPT.
+ * @config  {Object}        Base user config object (derrived by user config file + CL param overrides).
+ * @isReference  {Boolean}  True if running reference flow.
+ * @return {Promise}        Resolves when fs.writeFile has completed.
+ */
+function writeReferenceCreateConfig (config, isReference) {
+  return fs.writeFile(config.captureConfigFileName, JSON.stringify(decorateConfigForCapture(config, isReference)));
+}
+
+function delegateScenarios (config) {
+  // var screenshotNow = new Date();
+  // var screenshotDateTime = screenshotNow.getFullYear() + pad(screenshotNow.getMonth() + 1) + pad(screenshotNow.getDate()) + '-' + pad(screenshotNow.getHours()) + pad(screenshotNow.getMinutes()) + pad(screenshotNow.getSeconds());
+
+  // TODO: start chromy here?  Or later?  maybe later because maybe changing resoloutions doesn't work after starting?
+  // casper.start();
+
+  var scenarios = [];
+  var scenarioViews = [];
+
+  config.viewports.forEach(function (o, i) {
+    o.vIndex = i;
+  });
+
+  // casper.each(scenarios, function (casper, scenario, i) {
+  config.scenarios.forEach(function (scenario, i) {
+    // var scenarioLabelSafe = makeSafe(scenario.label);
+    scenario.sIndex = i;
+    scenarios.push(scenario);
+    // processScenario(scenario, scenarioLabelSafe, scenarioLabelSafe, viewports, bitmapsReferencePath, bitmapsTestPath, screenshotDateTime);
+
+    if (!config.isReference && scenario.hasOwnProperty('variants')) {
+      scenario.variants.forEach(function (variant) {
+        // var variantLabelSafe = makeSafe(variant.label);
+        variant._parent = scenario;
+        scenarios.push(scenario);
+        // processScenario(casper, variant, variantLabelSafe, scenarioLabelSafe, viewports, bitmapsReferencePath, bitmapsTestPath, screenshotDateTime);
+      });
+    }
+  });
+
+  scenarios.forEach(function (scenario) {
+    config.viewports.forEach(function (viewport) {
+      scenarioViews.push({
+        scenario: scenario,
+        viewport: viewport,
+        config: config
+      });
+    });
+  });
+
+  // return Promise.reject(new Error('note yet delegating'));
+  return pMap(scenarioViews, runChromy, { concurrency: 2 });
+}
+
+function pad (number) {
+  var r = String(number);
+  if (r.length === 1) {
+    r = '0' + r;
+  }
+  return r;
 }
 
 module.exports = function (config, isReference) {
+  if (/chromy/.test(config.engine)) {
+    return delegateScenarios(decorateConfigForCapture(config, isReference));
+    // return runChromy(decorateConfigForCapture(config, isReference));
+  }
+
   return writeReferenceCreateConfig(config, isReference).then(function () {
     var tests = [path.join(config.backstop, GENERATE_BITMAPS_SCRIPT)];
     var casperChild = runCasper(config, tests);
---
+++ core/util/runChromy.js
@@ -0,0 +1,424 @@
+const Chromy = require('chromy');
+// const path = require('path');
+
+var fs = require('fs');
+var cwd = fs.workingDirectory;
+
+var scriptTimeout = 20000;
+
+module.exports = function (args) {
+  var scenario = args.scenario;
+  var viewport = args.viewport;
+  var config = args.config;
+
+  var scenarioLabelSafe = makeSafe(scenario.label);
+  var variantOrScenarioLabelSafe = scenario._parent ? makeSafe(scenario._parent.label) : scenarioLabelSafe;
+  var viewportLabelSafe = makeSafe(viewport.label);
+
+  return processScenarioView(scenario, variantOrScenarioLabelSafe, scenarioLabelSafe, viewport, viewportLabelSafe, config);
+};
+
+/**
+ * [processScenarioView description]
+ * @param  {[type]} scenario               [description]
+ * @param  {[type]} scenarioOrVariantLabel [description]
+ * @param  {[type]} scenarioLabel          [description]
+ * @param  {[type]} viewport               [description]
+ * @param  {[type]} config                 [description]
+ * @return {[type]}                        [description]
+ */
+function processScenarioView (scenario, scenarioOrVariantLabel, scenarioLabel, viewport, config) {
+  console.log('processScenarioView >>', scenarioLabel, viewport.name)
+
+  var DOCUMENT_SELECTOR = 'document';
+
+  if (!config.paths) {
+    config.paths = {};
+  }
+
+  var outputFormat = '.' + (config.outputFormat && config.outputFormat.match(/jpg|jpeg/) || 'png');
+  var bitmapsReferencePath = config.paths.bitmaps_reference || 'bitmaps_reference';
+  var bitmapsTestPath = config.paths.bitmaps_test || 'bitmaps_test';
+  var casperScriptsPath = config.paths.casper_scripts || null;
+  var comparePairsFileName = config.paths.tempCompareConfigFileName;
+  var screenshotDateTime = config.screenshotDateTime;
+  var configId = config.id || genHash(config.backstopConfigFileName);
+  var fileNameTemplate = config.fileNameTemplate || '{configId}_{scenarioLabel}_{selectorIndex}_{selectorLabel}_{viewportIndex}_{viewportLabel}';
+
+
+  var selectorNotFoundPath = config.env.backstop + '/capture/resources/selectorNotFound_noun_164558_cc.png';
+  var hiddenSelectorPath = config.env.backstop + '/capture/resources/hiddenSelector_noun_63405.png';
+
+  var isReference = config.isReference;
+  if (isReference) {
+    console.log('CREATING NEW REFERENCE FILES');
+  }
+
+  if (!config.paths) {
+    config.paths = {};
+  }
+  var compareConfig = {testPairs: []};
+
+
+//=====================EARLY RETURN======================
+  // return Promise.reject(new Error('chromy does not yet do the thing'));
+  return Promise.resolve(new Error('chromy does not yet do the thing'));
+//=====================EARLY RETURN======================
+
+  /**
+   * =================
+   * CLI BEHAVIORS
+   * =================
+   */
+
+  // var isReference = config.isReference;
+  // if (isReference) {
+  //   console.log('CREATING NEW REFERENCE FILES');
+  // }
+
+  // // verbose console errors
+  // if (config.debug) {
+  //   console.log('Debug is enabled!');
+  //   casper.on('page.error', function (msg, trace) {
+  //     this.echo('Remote Error >    ' + msg, 'error');
+  //     this.echo('file:     ' + trace[0].file, 'WARNING');
+  //     this.echo('line:     ' + trace[0].line, 'WARNING');
+  //     this.echo('function: ' + trace[0]['function'], 'WARNING');
+  //   });
+  // }
+
+  // // verbose resource monitoring
+  // casper.on('resource.received', function (resource) {
+  //   var status = resource.status;
+  //   if (status >= 400) {
+  //     casper.log('remote error > ' + resource.url + ' failed to load (' + status + ')', 'error');
+  //   }
+  // });
+
+
+
+  /**
+   *  =============
+   *  TEST UTILS
+   *  =============
+   */
+
+
+  // TODO: REQUIRED!!!
+  // // set up consoleBuffer
+  var consoleBuffer = '';
+  // casper.on('remote.message', function (message) {
+  //   casper.echo(message);
+  //   consoleBuffer = consoleBuffer + '\n' + message;
+  // });
+
+
+
+
+  /**
+   *  =============
+   *  START CHROMY SESSION
+   *  =============
+   */
+
+    // TODO: start chromy in blank and set viewport
+    // this.viewport(vp.width || vp.viewport.width, vp.height || vp.viewport.height);
+
+    // var onBeforeScript = scenario.onBeforeScript || config.onBeforeScript;
+    // if (onBeforeScript) {
+    //   require(getScriptPath(onBeforeScript))(casper, scenario, vp, isReference);
+    // }
+
+    // if (casper.cli.options.user && casper.cli.options.password) {
+    //   console.log('Auth User via CLI: ' + casper.cli.options.user);
+    //   casper.setHttpAuth(casper.cli.options.user, casper.cli.options.password);
+    // }
+
+    // var url = scenario.url;
+    // if (isReference && scenario.referenceUrl) {
+    //   url = scenario.referenceUrl;
+    // }
+
+    // this.thenOpen(url, function () {
+    //   casper.waitFor(
+    //     function () { // test
+    //       var readyEvent = scenario.readyEvent || config.readyEvent;
+    //       if (!readyEvent) {
+    //         return true;
+    //       }
+    //       var regExReadyFlag = new RegExp(readyEvent, 'i');
+    //       return consoleBuffer.search(regExReadyFlag) > -1;
+    //     },
+    //     function () { // on done
+    //       consoleBuffer = '';
+    //       casper.echo('Ready event received.');
+    //     },
+    //     function () {
+    //       casper.echo('Error while waiting for ready event.');
+    //     }, // on timeout
+    //     scriptTimeout
+    //   );
+    //   casper.wait(scenario.delay || 1);
+    // });
+
+    // casper.then(function () {
+    //   this.echo('Current location is ' + url, 'info');
+
+    //   if (config.debug) {
+    //     var src = this.evaluate(function () {
+    //       return document.body.outerHTML;
+    //     });
+    //     this.echo(src);
+    //   }
+    // });
+
+    // Custom casperjs scripting after ready event and delay
+
+    /* ============
+      onReadyScript files should export a module like so:
+
+      module.exports = function(casper, scenario, vp) {
+        // run custom casperjs code
+      };
+    ============ */
+    casper.then(function () {
+
+      var onReadyScript = scenario.onReadyScript || config.onReadyScript;
+      if (onReadyScript) {
+        require(getScriptPath(onReadyScript))(casper, scenario, vp, isReference);
+      }
+    });
+
+    this.then(function () {
+      this.echo('Capturing screenshots for ' + makeSafe(vp.name) + ' (' + (vp.width || vp.viewport.width) + 'x' + (vp.height || vp.viewport.height) + ')', 'info');
+
+      // HIDE SELECTORS WE WANT TO AVOID
+      if (scenario.hasOwnProperty('hideSelectors')) {
+        scenario.hideSelectors.forEach(function (o, i, a) {
+          casper.evaluate(function (o) {
+            Array.prototype.forEach.call(document.querySelectorAll(o), function (s, j) {
+              s.style.visibility = 'hidden';
+            });
+          }, o);
+        });
+      }
+
+      // REMOVE UNWANTED SELECTORS FROM RENDER TREE
+      if (scenario.hasOwnProperty('removeSelectors')) {
+        scenario.removeSelectors.forEach(function (o, i, a) {
+          casper.evaluate(function (o) {
+            Array.prototype.forEach.call(document.querySelectorAll(o), function (s, j) {
+              s.style.display = 'none';
+              s.classList.add('__86d');
+            });
+          }, o);
+        });
+      }
+
+      // CREATE SCREEN SHOTS AND TEST COMPARE CONFIGURATION (CONFIG FILE WILL BE SAVED WHEN THIS PROCESS RETURNS)
+      // If no selectors are provided then set the default DOCUMENT_SELECTOR
+      if (!scenario.hasOwnProperty('selectors') || !scenario.selectors.length) {
+        scenario.selectors = [DOCUMENT_SELECTOR];
+      }
+
+      if (scenario.selectorExpansion) {
+        scenario.selectorsExpanded = scenario.selectors.reduce(function (acc, selector) {
+          if (selector === DOCUMENT_SELECTOR) {
+            return acc.concat([DOCUMENT_SELECTOR]);
+          }
+
+          var expandedSelector = casper.evaluate(function (selector) {
+            return [].slice.call(document.querySelectorAll(selector)).map(function (element, expandedIndex) {
+              var indexPartial = '__n' + expandedIndex;
+
+              if (element.classList.contains('__86d')) {
+                return '';
+              }
+
+              if (!expandedIndex) {
+                // only first element is used for screenshots -- even if multiple instances exist.
+                // therefore index 0 does not need extended qualification.
+                return selector;
+              }
+
+              // update all matching selectors with additional indexPartial class
+              element.classList.add(indexPartial);
+
+              // return array of fully-qualified classnames
+              return selector + '.' + indexPartial;
+            });
+          }, selector);
+
+          // concat arrays of fully-qualified classnames
+          return acc.concat(expandedSelector);
+        }, []).filter(function (selector) {
+          return selector !== '';
+        });
+      } else {
+        scenario.selectorsExpanded = scenario.selectors;
+      }
+
+      scenario.selectorsExpanded.forEach(function (o, i, a) {
+        var cleanedSelectorName = o.replace(/[^a-z0-9_-]/gi, ''); // remove anything that's not a letter or a number
+
+        var fileName = getFilename(scenario.sIndex, scenarioOrVariantLabel, i, cleanedSelectorName, viewportIndex, vp.name);
+
+        var referenceFilePath = bitmapsReferencePath + '/' + getFilename(scenario.sIndex, scenarioLabel, i, cleanedSelectorName, viewportIndex, vp.name);
+        var testFilePath = bitmapsTestPath + '/' + screenshotDateTime + '/' + fileName;
+
+        var filePath = (isReference) ? referenceFilePath : testFilePath;
+
+        captureScreenshot(casper, filePath, o);
+
+        if (!isReference) {
+            var requireSameDimensions;
+            if (scenario.requireSameDimensions !== undefined) {
+                requireSameDimensions = scenario.requireSameDimensions;
+            } else if (config.requireSameDimensions !== undefined) {
+                requireSameDimensions = config.requireSameDimensions;
+            } else {
+                requireSameDimensions = config.defaultRequireSameDimensions;
+            }
+          compareConfig.testPairs.push({
+            reference: referenceFilePath,
+            test: testFilePath,
+            selector: o,
+            fileName: fileName,
+            label: scenario.label,
+            requireSameDimensions: requireSameDimensions,
+            misMatchThreshold: getMisMatchThreshHold(scenario)
+          });
+        }
+        // casper.echo('remote capture to > '+filePath,'info');
+      });// end topLevelModules.forEach
+    });
+  // });// end casper.each viewports
+}
+
+
+// vvv HELPERS vvv
+
+function getMisMatchThreshHold (scenario) {
+  if (typeof scenario.misMatchThreshold !== 'undefined') { return scenario.misMatchThreshold; }
+  if (typeof config.misMatchThreshold !== 'undefined') { return config.misMatchThreshold; }
+  return config.defaultMisMatchThreshold;
+}
+
+function captureScreenshot (casper, filePath, selector) {
+  if (selector === 'body:noclip' || selector === 'document') {
+    casper.capture(filePath);
+  } else if (casper.exists(selector)) {
+    if (casper.visible(selector)) {
+      casper.captureSelector(filePath, selector);
+    } else {
+      fs.write(filePath, fs.read(hiddenSelectorPath, 'b'), 'b');
+    }
+  } else {
+    fs.write(filePath, fs.read(selectorNotFoundPath, 'b'), 'b');
+  }
+}
+
+// function complete () {
+//   var compareConfigJSON = {compareConfig: compareConfig};
+//   fs.write(comparePairsFileName, JSON.stringify(compareConfigJSON, null, 2), 'w');
+//   console.log('Comparison config file updated.');
+// }
+
+// function pad (number) {
+//   var r = String(number);
+//   if (r.length === 1) {
+//     r = '0' + r;
+//   }
+//   return r;
+// }
+
+function getScriptPath (scriptFilePath) {
+  var scriptPath = ensureFileSuffix(scriptFilePath, 'js');
+
+  if (casperScriptsPath) {
+    scriptPath = glueStringsWithSlash(casperScriptsPath, scriptPath);
+  }
+
+  // make sure it's there...
+  if (!fs.isFile(scriptPath)) {
+    casper.echo(scriptPath + ' was not found.', 'ERROR');
+    return;
+  }
+
+  return cwd + fs.separator + scriptPath;
+}
+
+function ensureFileSuffix (filename, suffix) {
+  var re = new RegExp('\.' + suffix + '$', ''); // eslint-disable-line no-useless-escape
+
+  return filename.replace(re, '') + '.' + suffix;
+}
+
+// merge both strings while soft-enforcing a single slash between them
+function glueStringsWithSlash (stringA, stringB) {
+  return stringA.replace(/\/$/, '') + '/' + stringB.replace(/^\//, '');
+}
+
+function genHash (str) {
+  var hash = 0;
+  var i;
+  var chr;
+  var len;
+  if (!str) return hash;
+  str = str.toString();
+  for (i = 0, len = str.length; i < len; i++) {
+    chr = str.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  // return a string and replace a negative sign with a zero
+  return hash.toString().replace(/^-/, 0);
+}
+
+function makeSafe (str) {
+  return str.replace(/[ /]/g, '_');
+}
+
+function getFilename (scenarioIndex, scenarioLabel, selectorIndex, selectorLabel, viewportIndex, viewportLabel) {
+  var fileName = fileNameTemplate
+    .replace(/\{configId\}/, configId)
+    .replace(/\{scenarioIndex\}/, scenarioIndex)
+    .replace(/\{scenarioLabel\}/, scenarioLabel)
+    .replace(/\{selectorIndex\}/, selectorIndex)
+    .replace(/\{selectorLabel\}/, selectorLabel)
+    .replace(/\{viewportIndex\}/, viewportIndex)
+    .replace(/\{viewportLabel\}/, makeSafe(viewportLabel))
+    .replace(/[^a-z0-9_-]/gi, ''); // remove anything that's not a letter or a number or dash or underscore.
+
+  var extRegExp = new RegExp(outputFormat + '$', 'i');
+  if (!extRegExp.test(fileName)) {
+    fileName = fileName + outputFormat;
+  }
+  return fileName;
+}
+
+
+
+
+// function testChromy (config) {
+//   const chromy = new Chromy({additionalChromeFlags: ['--window-size=2000,1000'], visible:true});
+//   let p = chromy.chain()
+//     .goto('https://garris.github.io/BackstopJS/')
+//     .screenshot()
+//     .result((png) => {
+//       fs.writeFile(config.env.projectPath + '/out.png', png);
+//     })
+
+//     // .evaluate(() => {
+//     //   return document.querySelector('#fsl').innerHTML;
+//     // })
+//     // .result((r) => console.log('>>>',r.toString()))
+//     .end()
+//     .then(_ => chromy.close())
+//     .catch(e => chromy.close());
+
+//   return p.then(() => {
+//     console.log('CHROMY DONE vvv');
+//     return Promise.reject(new Error('chromy does not yet do the thing'));
+//   });
+// }
