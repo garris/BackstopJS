829c9c4b7d97f483d586cde81ddf7b253de32139
Anton Kulahin <titusja@yandex.ru>
2017-09-01 18:08:35 +0300

    filter for approve

--- core/command/approve.js
+++ core/command/approve.js
@@ -3,31 +3,36 @@ var path = require('path');
 var map = require('p-map');
 
 var FAILED_DIFF_RE = /^failed_diff_/;
+var FILTER_DEFAULT = /[\d\w]*/;
 
 // This task will copy ALL test bitmap files (from the most recent test directory) to the reference directory overwritting any exisiting files.
 module.exports = {
-  execute: function (config) {
-    // TODO:  IF Exists config.bitmaps_test  &&  list.length > 0n  (otherwise throw)
-    console.log('Copying from ' + config.bitmaps_test + ' to ' + config.bitmaps_reference + '.');
-    return fs.readdir(config.bitmaps_test, (err, list) => {
-      if (err) {
-        console.log(err.stack);
-      }
-      var src = path.join(config.bitmaps_test, list[list.length - 1]);
-      return fs.readdir(src, (err, files) => {
-        if (err) {
-          console.log(err.stack);
-        }
-        console.log('The following files will be promoted to reference...');
-        return map(files, (file) => {
-          if (FAILED_DIFF_RE.test(file)) {
-            file = file.replace(FAILED_DIFF_RE, '');
-            console.log('> ', file);
-            return fs.copy(path.join(src, file), path.join(config.bitmaps_reference, file));
-          }
-          return true;
+    execute: function(config) {
+        // TODO:  IF Exists config.bitmaps_test  &&  list.length > 0n  (otherwise throw)
+        console.log('Copying from ' + config.bitmaps_test + ' to ' + config.bitmaps_reference + '.');
+        return fs.readdir(config.bitmaps_test, (err, list) => {
+            if (err) {
+                console.log(err.stack);
+            }
+            var src = path.join(config.bitmaps_test, list[list.length - 1]);
+            return fs.readdir(src, (err, files) => {
+                if (err) {
+                    console.log(err.stack);
+                }
+                console.log('The following files will be promoted to reference...');
+                return map(files, (file) => {
+                    let imageFilter = FILTER_DEFAULT;
+                    if (config.args && config.args.filter) {
+                        imageFilter = new RegExp("_" + config.args.filter + "_");
+                    }
+                    if (FAILED_DIFF_RE.test(file) && imageFilter.test(file)) {
+                        file = file.replace(FAILED_DIFF_RE, '');
+                        console.log('> ', file);
+                        return fs.copy(path.join(src, file), path.join(config.bitmaps_reference, file));
+                    }
+                    return true;
+                });
+            });
         });
-      });
-    });
-  }
+    }
 };
