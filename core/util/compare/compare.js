103e122107828df6a21c65ff43bd15c6ce7893d5
Vladislav Dekov <vdekoff@gmail.com>
2017-10-18 21:16:30 +0300

    Execute each images comparison in a separate child processes (#561)

--- core/util/compare/compare.js
+++ core/util/compare/compare.js
@@ -1,7 +1,33 @@
 var compareHashes = require('./compare-hash');
 var compareResemble = require('./compare-resemble');
+var storeFailedDiff = require('./store-failed-diff.js');
+var logger = require('./../logger')('compare');
 
-module.exports = function (referencePath, testPath, misMatchThreshold, resembleOutputSettings, requireSameDimensions) {
-  return compareHashes(referencePath, testPath)
-     .catch(() => compareResemble(referencePath, testPath, misMatchThreshold, resembleOutputSettings, requireSameDimensions));
+process.on('message', compare);
+
+function compare (data) {
+  var {referencePath, testPath, resembleOutputSettings, pair} = data;
+  var promise = compareHashes(referencePath, testPath)
+    .catch(() => compareResemble(referencePath, testPath, pair.misMatchThreshold, resembleOutputSettings, pair.requireSameDimensions));
+  promise
+    .then(function (data) {
+      pair.diff = data;
+      pair.status = 'pass';
+      logger.success('OK: ' + pair.label + ' ' + pair.fileName);
+      return sendMessage(pair);
+    })
+    .catch(function (data) {
+      pair.diff = data;
+      pair.status = 'fail';
+      logger.error('ERROR { requireSameDimensions: ' + (data.requireSameDimensions ? 'true' : 'false') + ', size: ' + (data.isSameDimensions ? 'ok' : 'isDifferent') + ', content: ' + data.misMatchPercentage + '%, threshold: ' + pair.misMatchThreshold + '% }: ' + pair.label + ' ' + pair.fileName);
+
+      return storeFailedDiff(testPath, data).then(function (compare) {
+        pair.diffImage = compare;
+        return sendMessage(pair);
+      });
+    });
 };
+
+function sendMessage (data) {
+  process.send(data);
+}
