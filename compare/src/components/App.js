270618e911efef1b8fa274de1f77c2f581c61c99
Gabriele Mantovani <gabriele.mantovani92@gmail.com>
2017-10-23 11:53:58 +0200

    Clean code + setup prettier on precommit

--- compare/src/components/App.js
+++ compare/src/components/App.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import styled from 'styled-components';
-import { StickyContainer, Sticky } from 'react-sticky';
+import React from 'react'
+import styled from 'styled-components'
+import { StickyContainer, Sticky } from 'react-sticky'
 
-import Header from './ecosystems/Header';
-import List from './ecosystems/List';
+import Header from './ecosystems/Header'
+import List from './ecosystems/List'
 
 const Wrapper = styled.section`
   padding: 0 30px;
-`;
+`
 
 export default class App extends React.Component {
-  render () {
+  render() {
     return (
-    <StickyContainer>
-      <Sticky topOffset={200}>
-            {
-              ({ isSticky, wasSticky, style, distanceFromTop, distanceFromBottom, calculatedHeight }) => {
-                console.log({ isSticky, wasSticky, style, distanceFromTop, distanceFromBottom, calculatedHeight });
-                return <Header style={style} />
-              }
-            }
-      </Sticky>
-      <Wrapper>
-        <List />
-      </Wrapper>
-    </StickyContainer>
-      );
+      <StickyContainer>
+        <Sticky topOffset={200}>
+          {({
+            isSticky,
+            wasSticky,
+            style,
+            distanceFromTop,
+            distanceFromBottom,
+            calculatedHeight
+          }) => {
+            return <Header style={style} />
+          }}
+        </Sticky>
+        <Wrapper>
+          <List />
+        </Wrapper>
+      </StickyContainer>
+    )
   }
 }
