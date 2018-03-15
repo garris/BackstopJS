88e1211aa996fbf37596adb0017b164adcfdea0a
krisdigital <info@krisdigital.com>
2018-03-15 20:37:23 +0100

    Implement missing Puppet functions

--- core/util/runPuppet.js
+++ core/util/runPuppet.js
@@ -1,12 +1,13 @@
 const puppeteer = require('puppeteer');
-// const writeFileSync = require('fs').writeFileSync;
+
 const fs = require('./fs');
 const path = require('path');
+const chalk = require('chalk');
 const ensureDirectoryPath = require('./ensureDirectoryPath');
+const injectBackstopTools = require('../../capture/backstopTools.js');
 
 const MIN_CHROME_VERSION = 62;
-const TEST_TIMEOUT = 30000;
-const CHROMY_STARTING_PORT_NUMBER = 9222;
+const TEST_TIMEOUT = 60000;
 const DEFAULT_FILENAME_TEMPLATE = '{configId}_{scenarioLabel}_{selectorIndex}_{selectorLabel}_{viewportIndex}_{viewportLabel}';
 const DEFAULT_BITMAPS_TEST_DIR = 'bitmaps_test';
 const DEFAULT_BITMAPS_REFERENCE_DIR = 'bitmaps_reference';
@@ -16,9 +17,6 @@ const BODY_SELECTOR = 'body';
 const DOCUMENT_SELECTOR = 'document';
 const NOCLIP_SELECTOR = 'body:noclip';
 const VIEWPORT_SELECTOR = 'viewport';
-
-const injectBackstopTools = require('../../capture/backstopTools.js');
-const BackstopException = require('../util/BackstopException.js');
 
 module.exports = function (args) {
   const scenario = args.scenario;
@@ -32,15 +30,7 @@ module.exports = function (args) {
   return processScenarioView(scenario, variantOrScenarioLabelSafe, scenarioLabelSafe, viewport, config, runId, assignedPort);
 };
 
-/**
- * [processScenarioView description]
- * @param  {[type]} scenario               [description]
- * @param  {[type]} variantOrScenarioLabelSafe [description]
- * @param  {[type]} scenarioLabelSafe          [description]
- * @param  {[type]} viewport               [description]
- * @param  {[type]} config                 [description]
- * @return {[type]}                        [description]
- */
+
 async function processScenarioView (scenario, variantOrScenarioLabelSafe, scenarioLabelSafe, viewport, config, runId) {
   if (!config.paths) {
     config.paths = {};
@@ -52,41 +42,25 @@ async function processScenarioView (scenario, variantOrScenarioLabelSafe, scenar
 
   const engineScriptsPath = config.env.engine_scripts || config.env.casper_scripts || config.env.engine_scripts_default;
   const isReference = config.isReference;
-  /**
-   *  =============
-   *  START CHROMY SESSION
-   *  =============
-   */
+
   const VP_W = viewport.width || viewport.viewport.width;
   const VP_H = viewport.height || viewport.viewport.height;
 
   const browser = await puppeteer.launch({ignoreHTTPSErrors: true});
   const page = await browser.newPage();
 
-  page.setViewport({width: VP_W, height: VP_H})
-  page.setDefaultNavigationTimeout(60000)
-  /**
-   * =================
-   * CLI BEHAVIORS
-   * =================
-   */
+  page.setViewport({width: VP_W, height: VP_H});
+  page.setDefaultNavigationTimeout(getEngineOption(config, 'waitTimeout', TEST_TIMEOUT));
 
   if (isReference) {
-    console.log('CREATING NEW REFERENCE FILES');
+    console.log(chalk.blue('CREATING NEW REFERENCE FILE'));
   }
 
-  /**
-   *  =============
-   *  TEST UTILS
-   *  =============
-   */
-
   // --- set up console output ---
-  // chromy.console(function (text, consoleObj) {
-  //   if (console[consoleObj.level]) {
-  //     console[consoleObj.level](PORT + ' ' + (consoleObj.level).toUpperCase() + ' > ', text);
-  //   }
-  // });
+  page.on('console', msg => {
+    for (let i = 0; i < msg.args().length; ++i)
+      console.log(`Browser Console Log ${i}: ${msg.args()[i]}`);
+  });
 
   let chromeVersion = await page.evaluate(_ => {
     let v = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
@@ -97,20 +71,7 @@ async function processScenarioView (scenario, variantOrScenarioLabelSafe, scenar
     console.warn(`***WARNING! CHROME VERSION ${MIN_CHROME_VERSION} OR GREATER IS REQUIRED. PLEASE UPDATE YOUR CHROME APP!***`);
   }
 
-  /**
-   *  =============
-   *  IMPLEMENT TEST CONFIGS
-   *  =============
-   */
-
   // --- BEFORE SCRIPT ---
-  /* ============
-    onBeforeScript files should export a module like so:
-
-    module.exports = function(renderer, scenario, vp) {
-      // run custom renderer (casper or chromy) code
-    };
-  ============ */
   var onBeforeScript = scenario.onBeforeScript || config.onBeforeScript;
   if (onBeforeScript) {
     var beforeScriptPath = path.resolve(engineScriptsPath, onBeforeScript);
@@ -133,12 +94,13 @@ async function processScenarioView (scenario, variantOrScenarioLabelSafe, scenar
   //  --- WAIT FOR READY EVENT ---
   var readyEvent = scenario.readyEvent || config.readyEvent;
   if (readyEvent) {
-    await page
-      .evaluate(`window._readyEvent = '${readyEvent}'`)
-      // .wait(_ => {
-//         return window._backstopTools.hasLogged(window._readyEvent);
-//       })
-      .evaluate(_ => console.info('readyEvent ok'));
+    await page.evaluate(`window._readyEvent = '${readyEvent}'`)
+
+    await page.waitForFunction(() => {
+      return window._backstopTools.hasLogged(window._readyEvent);
+    });
+      
+    await page.evaluate(_ => console.info('readyEvent ok'));
   }
 
   // --- WAIT FOR SELECTOR ---
@@ -148,40 +110,40 @@ async function processScenarioView (scenario, variantOrScenarioLabelSafe, scenar
   //
 
   // --- DELAY ---
-  // chromy.wait(scenario.delay || 0);
+  if (scenario.delay > 0) {
+    await page.waitFor(scenario.delay);
+  }
 
+  //--- REMOVE SELECTORS ---
+  if (scenario.hasOwnProperty('removeSelectors')) {
+    const removeSelectors = async () => {
+      return Promise.all(
+        scenario.removeSelectors.map(async (selector) => {
+          await page
+            .evaluate(`window._backstopSelector = '${selector}'`);
 
-  // --- REMOVE SELECTORS ---
-  // if (scenario.hasOwnProperty('removeSelectors')) {
-  //   scenario.removeSelectors.forEach(function (selector) {
-  //     page
-  //     .evaluate(`window._backstopSelector = '${selector}'`)
-  //     .evaluate(
-  //       () => {
-  //         Array.prototype.forEach.call(document.querySelectorAll(window._backstopSelector), function (s, j) {
-  //           s.style.display = 'none';
-  //           s.classList.add('__86d');
-  //         });
-  //       }
-  //     );
-  //   });
-  // }
+          await page
+            .evaluate(() => {
+              Array.prototype.forEach.call(document.querySelectorAll(window._backstopSelector), function (s, j) {
+                s.style.display = 'none';
+                s.classList.add('__86d');
+              });
+            });
+        })
+      );
+    }
+    
+    await removeSelectors();
+  }
 
   //  --- ON READY SCRIPT ---
-  /* ============
-    onReadyScript files should export a module like so:
-
-    module.exports = function(renderer, scenario, vp, isReference) {
-      // run custom renderer (casper or chromy) code
-    };
-  ============ */
   var onReadyScript = scenario.onReadyScript || config.onReadyScript;
   if (onReadyScript) {
     var readyScriptPath = path.resolve(engineScriptsPath, onReadyScript);
     if (fs.existsSync(readyScriptPath)) {
-      require(readyScriptPath)(page, scenario, viewport, isReference, browser);
+      await require(readyScriptPath)(page, scenario, viewport, isReference, browser);
     } else {
-      console.warn(PORT, 'WARNING: script not found: ' + readyScriptPath);
+      console.warn('WARNING: script not found: ' + readyScriptPath);
     }
   }
 
@@ -189,21 +151,24 @@ async function processScenarioView (scenario, variantOrScenarioLabelSafe, scenar
   await injectBackstopTools(page);
 
   // --- HIDE SELECTORS ---
-  // if (scenario.hasOwnProperty('hideSelectors')) {
-  //   scenario.hideSelectors.forEach(function (selector) {
-  //     page
-  //     .evaluate(`window._backstopSelector = '${selector}'`)
-  //     .evaluate(
-  //       () => {
-  //         Array.prototype.forEach.call(document.querySelectorAll(window._backstopSelector), function (s, j) {
-  //           s.style.visibility = 'hidden';
-  //         });
-  //       }
-  //     );
-  //   });
-  // }
-  // CREATE SCREEN SHOTS AND TEST COMPARE CONFIGURATION (CONFIG FILE WILL BE SAVED WHEN THIS PROCESS RETURNS)
-  // this.echo('Capturing screenshots for ' + makeSafe(vp.name) + ' (' + (vp.width || vp.viewport.width) + 'x' + (vp.height || vp.viewport.height) + ')', 'info');
+  if (scenario.hasOwnProperty('hideSelectors')) {
+    const hideSelectors = async () => {
+      return Promise.all(
+        scenario.hideSelectors.map(async (selector) => {
+          await page
+            .evaluate(`window._backstopSelector = '${selector}'`)
+
+          await page
+            .evaluate(() => {
+              Array.prototype.forEach.call(document.querySelectorAll(window._backstopSelector), function (s, j) {
+                s.style.visibility = 'hidden';
+              });
+            });
+        })
+      );
+    }
+    await hideSelectors()
+  }
 
   // --- HANDLE NO-SELECTORS ---
   if (!scenario.hasOwnProperty('selectors') || !scenario.selectors.length) {
@@ -234,12 +199,6 @@ async function processScenarioView (scenario, variantOrScenarioLabelSafe, scenar
       };
     })
 
-  // If an error occurred then resolve with an error.
-  // .catch(e => {
-  //   console.log(e);
-  //   resolve(new BackstopException('Puppeteer error', scenario, viewport, e))
-  // });
-
   return new Promise((resolve, reject) => {
     resolve(delegateSelectors(
       page,
@@ -255,16 +214,7 @@ async function processScenarioView (scenario, variantOrScenarioLabelSafe, scenar
   });
 }
 
-// vvv HELPERS vvv
-/**
- * [delegateSelectors description]
- * @param  {[type]} chromy                     [description]
- * @param  {[type]} scenario                   [description]
- * @param  {[type]} viewport                   [description]
- * @param  {[type]} variantOrScenarioLabelSafe [description]
- * @param  {[type]} config                     [description]
- * @return {[type]}                            [description]
- */
+
 function delegateSelectors (page, browser, scenario, viewport, variantOrScenarioLabelSafe, scenarioLabelSafe, config, selectors, selectorMap) {
   const fileNameTemplate = config.fileNameTemplate || DEFAULT_FILENAME_TEMPLATE;
   const configId = config.id || genHash(config.backstopConfigFileName);
@@ -316,14 +266,14 @@ function delegateSelectors (page, browser, scenario, viewport, variantOrScenario
   });
 
   if (captureDocument) {
-    captureJobs.push(function () { return captureScreenshot(page, browser, null, captureDocument, selectorMap, config, []); });
+    captureJobs.push(function () { return captureScreenshot(page, browser, captureDocument, selectorMap, config, []); });
   }
   // TODO: push captureViewport into captureList (instead of calling captureScreenshot()) to improve perf.
   if (captureViewport) {
-    captureJobs.push(function () { return captureScreenshot(page, browser, null, captureViewport, selectorMap, config, []); });
+    captureJobs.push(function () { return captureScreenshot(page, browser, captureViewport, selectorMap, config, []); });
   }
   if (captureList.length) {
-    captureJobs.push(function () { return captureScreenshot(page, browser, null, null, selectorMap, config, captureList); });
+    captureJobs.push(function () { return captureScreenshot(page, browser, null, selectorMap, config, captureList); });
   }
 
   return new Promise(function (resolve, reject) {
@@ -348,10 +298,10 @@ function delegateSelectors (page, browser, scenario, viewport, variantOrScenario
     };
     next();
   }).then(function () {
-    console.log('Close Browser');
+    console.log(chalk.green('x Close Browser'));
     return browser.close();
   }).catch(function (err) {
-    console.log(err);
+    console.log(chalk.red(err));
     browser.close();
   }).then(_ => compareConfig);
 }
@@ -366,57 +316,28 @@ function delegateSelectors (page, browser, scenario, viewport, variantOrScenario
  */
 
 // TODO: remove filepath_
-async function captureScreenshot (page, browser, filePath_, selector, selectorMap, config, selectors) {
-  ensureDirectoryPath(selectorMap[selector].filePath);
-  await page
-    .screenshot({path: selectorMap[selector].filePath});
+async function captureScreenshot (page, browser, selector, selectorMap, config, selectors) {
+  const filePath = selectorMap[selector].filePath;
+  ensureDirectoryPath(filePath);
+  
+  if (selector === VIEWPORT_SELECTOR || selector === BODY_SELECTOR) {
+    // VIEWPORT screenshot
+    await page
+      .screenshot({path: filePath});
+  } else if (selector === NOCLIP_SELECTOR || selector === DOCUMENT_SELECTOR) {
+    // DOCUMENT screenshot
+    await page
+      .screenshot({
+        path: filePath,
+        fullPage: true
+      });
+  } else {
+    // OTHER-SELECTOR screenshot
+    // TODO....
+  }
 
   return new Promise (function (resolve, reject) {
     resolve();
-    // VIEWPORT screenshot
-    if (selector === VIEWPORT_SELECTOR || selector === BODY_SELECTOR) {
-
-    // DOCUMENT screenshot
-    } // else if (selector === NOCLIP_SELECTOR || selector === DOCUMENT_SELECTOR) {
-//       chromy.screenshotMultipleSelectors(['body'], saveSelector);
-//     // OTHER-SELECTOR screenshot
-//     } else {
-//       chromy.screenshotMultipleSelectors(selectors, saveSelector);
-//     }
-
-
-    // result helpers
-
-    // saveViewport: selectors will be `body` or `viewport` ONLY
-    // function saveViewport (buffer, selector) {
-    //   const filePath = selectorMap[selector].filePath;
-    //
-    //   ensureDirectoryPath(filePath);
-    //   return fs.writeFile(filePath, buffer);
-    // }
-
-    // saveSelector: selectorArr will contain any valid selector (not body or viewport).
-    // If body *is* found in selector arr then it was originally DOCUMENT_SELECTOR -- and it will be reset back to DOCUMENT_SELECTOR -- this is because chromy takes a Document shot when BODY is used.
-    // function saveSelector (err, buffer, index, selectorArr) {
-    //   let selector = selectorArr[index];
-    //   if (selector === BODY_SELECTOR) {
-    //     selector = DOCUMENT_SELECTOR;
-    //   }
-    //   const selectorProps = selectorMap[selector];
-    //   const filePath = selectorProps.filePath;
-    //   if (!selectorProps.exists) {
-    //     return fs.copy(config.env.backstop + SELECTOR_NOT_FOUND_PATH, filePath);
-    //   } else if (!selectorProps.isVisible) {
-    //     return fs.copy(config.env.backstop + HIDDEN_SELECTOR_PATH, filePath);
-    //   } else {
-    //     if (err) {
-    //       console.log('ChromyJS returned an unexpected error while attempting to capture a selector.', err);
-    //       return new Error(err);
-    //     }
-    //     ensureDirectoryPath(filePath);
-    //     return fs.writeFile(filePath, buffer);
-    //   }
-    // }
   });
 }
 
@@ -472,5 +393,12 @@ function getFilename (fileNameTemplate, outputFileFormatSuffix, configId, scenar
     fileName = fileName + outputFileFormatSuffix;
   }
   return fileName;
+}
+
+function getEngineOption(config, optionName, fallBack) {
+  if (typeof config.engineOptions === 'object' && config.engineOptions[optionName]) {
+    return config.engineOptions[optionName];
+  }
+  return fallBack;
 }
 
