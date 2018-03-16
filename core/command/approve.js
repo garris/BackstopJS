643e9b0ff0443412332c3a9a1345ffcc0a7f7b89
Enikő Nagy <eenagy@users.noreply.github.com>
2018-03-16 06:42:47 +0100

    add promise to approve (#642)

--- core/command/approve.js
+++ core/command/approve.js
@@ -10,30 +10,36 @@ module.exports = {
   execute: function (config) {
     // TODO:  IF Exists config.bitmaps_test  &&  list.length > 0n  (otherwise throw)
     console.log('Copying from ' + config.bitmaps_test + ' to ' + config.bitmaps_reference + '.');
-    return fs.readdir(config.bitmaps_test, (err, list) => {
-      if (err) {
-        console.log(err.stack);
-      }
-      var src = path.join(config.bitmaps_test, list[list.length - 1]);
-      return fs.readdir(src, (err, files) => {
+    return new Promise((resolve, reject) => {
+      fs.readdir(config.bitmaps_test, (err, list) => {
+
         if (err) {
           console.log(err.stack);
+          reject();
         }
-        console.log('The following files will be promoted to reference...');
-        return map(files, (file) => {
-          if (FAILED_DIFF_RE.test(file)) {
-            file = file.replace(FAILED_DIFF_RE, '');
-
-            let imageFilter = FILTER_DEFAULT;
-            if (config.args && config.args.filter) {
-              imageFilter = new RegExp(config.args.filter);
-            }
-            if (imageFilter.test(file)) {
-              console.log('> ', file);
-              return fs.copy(path.join(src, file), path.join(config.bitmaps_reference, file));
-            }
+        var src = path.join(config.bitmaps_test, list[list.length - 1]);
+        return fs.readdir(src, (err, files) => {
+          if (err) {
+            console.log(err.stack);
+            reject();
           }
-          return true;
+          console.log('The following files will be promoted to reference...');
+
+          return map(files, (file) => {
+            if (FAILED_DIFF_RE.test(file)) {
+              file = file.replace(FAILED_DIFF_RE, '');
+
+              let imageFilter = FILTER_DEFAULT;
+              if (config.args && config.args.filter) {
+                imageFilter = new RegExp(config.args.filter);
+              }
+              if (imageFilter.test(file)) {
+                console.log('> ', file);
+                return fs.copy(path.join(src, file), path.join(config.bitmaps_reference, file));
+              }
+            }
+            return true;
+          }).then(resolve).catch(reject);
         });
       });
     });
