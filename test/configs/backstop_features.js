cf3254a905eafe4c623059a8f84d20f2463677c8
Garris Shipon <garris@me.com>
2018-03-25 19:05:12 -0700

    increase backstop_features asyncCaptureLimit to 5

--- test/configs/backstop_features.js
+++ test/configs/backstop_features.js
@@ -132,7 +132,7 @@ module.exports = {
   report: ['browser'],
   engine: ENGINE,
   engineFlags: [],
-  asyncCaptureLimit: 1,
+  asyncCaptureLimit: 5,
   asyncCompareLimit: 50,
   debug: false,
   debugWindow: false
