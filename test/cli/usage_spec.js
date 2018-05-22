c98c37dce7aeda5ec96c2c52450b33a4362a6873
Xingxin Zeng <star_yes@qq.com>
2018-05-22 16:00:50 +0800

    Fix the unit test

--- test/cli/usage_spec.js
+++ test/cli/usage_spec.js
@@ -1,23 +1,24 @@
 var usage = require('../../cli/usage');
 var assert = require('assert');
 
-var expectedUsage = "Welcome to BackstopJS 2.1.4 CLI\n\
-\n\
-Commands:\n\
-    reference      Create reference screenshots of your web content at multiple screen sizes.\n\
-    test           Create test screenshots of your web content and compare against the set you created using `backstop reference`.\n\
-    genConfig      Generate a configuration file boilerplate in your current directory. PLEASE NOTE: this will force overwrite any existing config.\n\
-    openReport     View your last test screenshots in your browser.\n\
-\n\
-Options:\n\
-    -h, --help     Display usage\n\
-    -v, --version  Display version\n\
-    -i             Incremental reference generation\n\
-\n\
-";
+var expectedUsage = `Welcome to BackstopJS 3.2.16 CLI
+
+Commands:
+    test           Create test screenshots and compare against the set you previously approved/referenced.
+    approve        Promotes all test bitmaps from last test run to reference bitmaps.
+    reference      Creates new reference screenshots. Deletes all existing reference files.
+    init           Generate BackstopJS boilerplate files in your CWD. NOTE: Overwrites existing config files!
+    openReport     View the last test report in your browser.
+
+Options:
+    -h, --help     Display usage
+    -v, --version  Display version
+    -i             incremental reference generation
+
+`;
 
 describe('the cli usage', function () {
   it('should print usage hints correctly', function () {
     assert.equal(usage, expectedUsage);
-    });
+  });
 });
