db2ec72fcc48dad71fa95479512468e0edcfe100
Brendon Barreto <brendondominic@gmail.com>
2018-10-08 13:59:25 -0300

    Revert "Bump dependencies: eslint"
    This reverts commit a62cc7bb24267ccbcab07cea9b6132d345b0d12c.

--- test/core/util/compare/compare_spec.js
+++ test/core/util/compare/compare_spec.js
@@ -5,10 +5,9 @@ describe('compare', function () {
   var compare;
   var compareHashes = sinon.stub();
   var compareResemble = sinon.stub();
-  var error = new Error();
 
   before(function () {
-    mockery.enable({ warnOnUnregistered: false, warnOnReplace: false, useCleanCache: true });
+    mockery.enable({warnOnUnregistered: false, warnOnReplace: false, useCleanCache: true});
     mockery.registerMock('./compare-hash', compareHashes);
     mockery.registerMock('./compare-resemble', compareResemble);
     compare = require('../../../../core/util/compare/compare');
@@ -25,21 +24,21 @@ describe('compare', function () {
 
   it.skip('should resolve if compare-hashes succeed', function () {
     compareHashes.withArgs('img1.png', 'img2.png').returns(Promise.resolve());
-    compareResemble.returns(Promise.reject(error));
+    compareResemble.returns(Promise.reject());
 
     return compare('img1.png', 'img2.png', 0, {});
   });
 
   it.skip('should resolve if compare-hashes fail, but compare-resemble succeeds', function () {
-    compareHashes.returns(Promise.reject(error));
+    compareHashes.returns(Promise.reject());
     compareResemble.withArgs('img1.png', 'img2.png', 0, {}).returns(Promise.resolve());
 
     return compare('img1.png', 'img2.png', 0, {});
   });
 
   it.skip('should reject if compare-hashes and compare-resemble fail', function (cb) {
-    compareHashes.returns(Promise.reject(error));
-    compareResemble.returns(Promise.reject(error));
+    compareHashes.returns(Promise.reject());
+    compareResemble.returns(Promise.reject());
 
     compare('img1.png', 'img2.png', 0, {}).catch(() => cb());
   });
--- test/core/util/makeConfig_it_spec.js
+++ test/core/util/makeConfig_it_spec.js
@@ -59,6 +59,6 @@ describe('make config', function () {
     assert(actualConfig.captureConfigFileName);
     delete actualConfig.captureConfigFileName;
 
-    assert.deepStrictEqual(actualConfig, expectedConfig);
+    assert.deepEqual(actualConfig, expectedConfig);
   });
 });
