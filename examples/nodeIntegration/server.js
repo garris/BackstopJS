73431920a7e6e33a1c021a9367184bdea46baec6
Gabe Herbert <gh@gabeherbert.com>
2019-07-07 23:10:47 -0700

    linting

--- examples/nodeIntegration/server.js
+++ examples/nodeIntegration/server.js
@@ -1,10 +1,10 @@
-const koa = require('koa');
+const Koa = require('koa');
 const serve = require('koa-serve');
 
-const app = new koa();
+const app = new Koa();
 
 app.use(serve('public'));
 
 app.listen(8000, () => {
-    console.log('Koa is listening at localhost:8000');
+  console.log('Koa is listening at localhost:8000');
 });
