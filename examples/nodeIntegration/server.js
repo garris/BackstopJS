60cdf756053db44b410156c0274531d3c86305af
Gabe Herbert <gh@gabeherbert.com>
2019-07-07 19:58:07 -0700

    update dependency versions; fix isssues

--- examples/nodeIntegration/server.js
+++ examples/nodeIntegration/server.js
@@ -1,10 +1,10 @@
 const koa = require('koa');
 const serve = require('koa-serve');
 
-const app = koa();
+const app = new koa();
 
 app.use(serve('public'));
 
 app.listen(8000, () => {
-    console.log('Koa is listening at localhost:8000')
+    console.log('Koa is listening at localhost:8000');
 });
