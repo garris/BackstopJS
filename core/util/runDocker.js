112162ce760a68d46c6d45e5c980a401bd8c150f
Josef Engelfrost <lists@josf.se>
2019-08-05 21:15:30 +0200

    Config object support with Docker (#988)
    * For Docker, convert config object literal to file
    
    * JSON config must end with .json

--- core/util/runDocker.js
+++ core/util/runDocker.js
@@ -1,11 +1,12 @@
 const { spawn } = require('child_process');
 const version = require('../../package').version;
+const fs = require('./fs');
 
 const DEFAULT_DOCKER_COMMAND_TEMPLATE = 'docker run --rm -it --mount type=bind,source="{cwd}",target=/src backstopjs/backstopjs:{version} {backstopCommand} {args}';
 
 module.exports.shouldRunDocker = (config) => config.args.docker;
 
-module.exports.runDocker = (config, backstopCommand) => {
+module.exports.runDocker = async (config, backstopCommand) => {
   if (config.args.docker) {
     // 0th element is node, 1st is backstop, 2nd may be command or an option like --config
     const args = process.argv.slice(2);
@@ -16,12 +17,22 @@ module.exports.runDocker = (config, backstopCommand) => {
       .join(' ')
       .replace(/--docker/, '--moby');
 
+    // We cannot pass object literals directly to Docker, so if the config is an object (and not a file path) we will output the config to a temporary file.
+    const tmpConfigFile = 'backstop.config-for-docker.json';
+
     // When calling BackstopJS from node config props will be overridden by the passed config object. e.g. backstop('test', {thisProp:'will be passed to config.args'})
-    // NOTE: passing config file name is supported -- passing actual config data is not supported.
     let configArgs = '';
     if (config.args && !config.args._) {
       configArgs = Object.keys(config.args)
-        .map(prop => `"--${prop}=${config.args[prop]}"`)
+        .map(async prop => {
+          if (prop === 'config' && typeof config.args[prop] === 'object') {
+            // If config is an object, export it to a json file
+            await fs.writeFile(tmpConfigFile, JSON.stringify(config.args[prop]));
+            config.args[prop] = tmpConfigFile;
+          }
+
+          return `"--${prop}=${config.args[prop]}"`;
+        })
         .join(' ')
         .replace(/--docker/, '--moby');
     }
@@ -43,7 +54,11 @@ module.exports.runDocker = (config, backstopCommand) => {
     return new Promise((resolve, reject) => {
       const dockerProcess = spawn(dockerCommand, { stdio: 'inherit', shell: true });
       dockerProcess.on('error', err => reject(err));
-      dockerProcess.on('exit', function (code, signal) {
+      dockerProcess.on('exit', async function (code, signal) {
+        if (!config.args.debug && config.args.config === tmpConfigFile) {
+          await fs.unlink(tmpConfigFile);
+        }
+
         if (code === 0) {
           resolve();
         } else {
