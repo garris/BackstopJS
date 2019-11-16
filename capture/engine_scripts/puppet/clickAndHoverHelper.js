366a085cf199cf4fe4ffdbfab87cb058efe39123
Kevin Gutiérrez <kevingutierrezg@gmail.com>
2019-11-16 12:51:36 -0500

    Pass vars to consts on engine_scripts (#1115)
    * Pass var to const on engine_scripts
    
    * Fix some consts that needs to be variable
    
    * Replace functions with arrow functions on engine_scripts

--- capture/engine_scripts/puppet/clickAndHoverHelper.js
+++ capture/engine_scripts/puppet/clickAndHoverHelper.js
@@ -1,9 +1,9 @@
 module.exports = async (page, scenario) => {
-  var hoverSelector = scenario.hoverSelectors || scenario.hoverSelector;
-  var clickSelector = scenario.clickSelectors || scenario.clickSelector;
-  var keyPressSelector = scenario.keyPressSelectors || scenario.keyPressSelector;
-  var scrollToSelector = scenario.scrollToSelector;
-  var postInteractionWait = scenario.postInteractionWait; // selector [str] | ms [int]
+  const hoverSelector = scenario.hoverSelectors || scenario.hoverSelector;
+  const clickSelector = scenario.clickSelectors || scenario.clickSelector;
+  const keyPressSelector = scenario.keyPressSelectors || scenario.keyPressSelector;
+  const scrollToSelector = scenario.scrollToSelector;
+  const postInteractionWait = scenario.postInteractionWait; // selector [str] | ms [int]
 
   if (keyPressSelector) {
     for (const keyPressSelectorItem of [].concat(keyPressSelector)) {
--- capture/engine_scripts/puppet/overrideCSS.js
+++ capture/engine_scripts/puppet/overrideCSS.js
@@ -4,9 +4,9 @@ module.exports = function (page, scenario) {
   // inject arbitrary css to override styles
   page.evaluate(`window._styleData = '${BACKSTOP_TEST_CSS_OVERRIDE}'`);
   page.evaluate(() => {
-    var style = document.createElement('style');
+    const style = document.createElement('style');
     style.type = 'text/css';
-    var styleNode = document.createTextNode(window._styleData);
+    const styleNode = document.createTextNode(window._styleData);
     style.appendChild(styleNode);
     document.head.appendChild(style);
   });
