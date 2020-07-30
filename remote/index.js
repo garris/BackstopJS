b126a89dd387f4c05c966c06ee27f95234916b57
Shankar Sridhar <shankar.utd@gmail.com>
2020-07-30 09:58:12 -0700

    Ability to switch between reference and test modes

--- remote/index.js
+++ remote/index.js
@@ -32,7 +32,7 @@ module.exports = function (app) {
     next();
   });
 
-  app.post('/dtest/:testId/:scenarioId', (req, res) => {
+  app.post(['/dtest/:testId/:scenarioId', '/dref/:testId/:scenarioId'], (req, res) => {
     app._backstop.testCtr++;
 
     if (!(req.params.testId in app._backstop.tests)) {
@@ -67,8 +67,8 @@ module.exports = function (app) {
       vid: app._backstop.testCtr
     };
 
-    // TODO: how to fork test vs reference
-    backstop('reference', { config, i: argsOptions.i }).then(
+    const command = req.path.includes('dtest') ? 'test' : 'reference';
+    backstop(command, { config, i: argsOptions.i }).then(
       () => {
         result.ok = true;
         res.send(JSON.stringify(result));
