e1fc6c254950f0fb5daba24a61d361f2abed0ef7
Garris Shpon <garris@me.com>
2020-08-10 13:40:15 -0700

    LINT!

--- test/configs/remote.js
+++ test/configs/remote.js
@@ -6,31 +6,31 @@ module.exports = {
     {
       label: 'webview',
       width: 1440,
-      height: 900,
-    },
+      height: 900
+    }
   ],
   onReadyScript: `puppet/onReady.js`,
   scenarios: [
     {
       label: '{testName}',
       url: '{origin}/backstop/dview/{testId}/{scenarioId}',
-      delay: 500,
-    },
+      delay: 500
+    }
   ],
   paths: {
     bitmaps_reference: 'backstop_data/bitmaps_reference',
     bitmaps_test: 'backstop_data/bitmaps_test',
     engine_scripts: 'backstop_data/engine_scripts',
     html_report: 'backstop_data/html_report',
-    ci_report: 'backstop_data/ci_report',
+    ci_report: 'backstop_data/ci_report'
   },
   report: [],
   engine: 'puppet',
   engineOptions: {
-    args: ['--no-sandbox'],
+    args: ['--no-sandbox']
   },
   asyncCaptureLimit: 10,
   asyncCompareLimit: 50,
   debug: false,
-  debugWindow: false,
+  debugWindow: false
 };
