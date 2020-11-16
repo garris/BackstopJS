c1d2b197b29daa017060f7f8ab0f215927c3eed6
Thorsten Krug <email@donkeymedia.eu>
2020-11-16 10:21:04 +0100

    Remove cookie domain as it could be munged.

--- capture/engine_scripts/puppet/loadCookies.js
+++ capture/engine_scripts/puppet/loadCookies.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports = async (page, scenario) => {
-  let cookies = [];
+  const cookies = [];
   const cookiePath = scenario.cookiePath;
 
   // READ COOKIES FROM FILE IF EXISTS
@@ -9,10 +9,14 @@ module.exports = async (page, scenario) => {
     cookies = JSON.parse(fs.readFileSync(cookiePath));
   }
 
-  // MUNGE COOKIE DOMAIN
+  // Inject cookie domain, url, secure from scenario url.
+  const url = new URL(scenario.url);
   cookies = cookies.map(cookie => {
-    cookie.url = 'https://' + cookie.domain;
-    delete cookie.domain;
+    cookie.url = `${url.protocol}//${url.hostname}`;
+    cookie.domain = url.hostname;
+    if(url.protocol === 'https:') {
+      cookie.secure = true;
+    }
     return cookie;
   });
 
