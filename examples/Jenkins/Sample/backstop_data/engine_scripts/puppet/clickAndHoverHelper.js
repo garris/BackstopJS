0bfdc5a87a8e006f976b9b5e06a67ddf0d351d3d
Maximilian <mfr@nzke.net>
2021-02-02 21:30:05 +0100

    refactor(waitFor): replaced .waitFor by its successor .waitForTimeout
    waitFor is deprecated and will be removed in a future release. See https://github.com/puppeteer/puppeteer/issues/6214 for details and how to migrate your code.

--- examples/Jenkins/Sample/backstop_data/engine_scripts/puppet/clickAndHoverHelper.js
+++ examples/Jenkins/Sample/backstop_data/engine_scripts/puppet/clickAndHoverHelper.js
@@ -4,16 +4,16 @@ module.exports = async (page, scenario) => {
   const postInteractionWait = scenario.postInteractionWait; // selector [str] | ms [int]
 
   if (hoverSelector) {
-    await page.waitFor(hoverSelector);
+    await page.waitForTimeout(hoverSelector);
     await page.hover(hoverSelector);
   }
 
   if (clickSelector) {
-    await page.waitFor(clickSelector);
+    await page.waitForTimeout(clickSelector);
     await page.click(clickSelector);
   }
 
   if (postInteractionWait) {
-    await page.waitFor(postInteractionWait);
+    await page.waitForTimeout(postInteractionWait);
   }
 };
