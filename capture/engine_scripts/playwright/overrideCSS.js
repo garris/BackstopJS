4b8914451055a7ad74d59f72133741115bd4c409
Joel Brown <joel.brown@odysseysr.com>
2021-11-17 06:50:04 -0600

    Updated CSS Override sample script

--- capture/engine_scripts/playwright/overrideCSS.js
+++ capture/engine_scripts/playwright/overrideCSS.js
@@ -1,14 +1,26 @@
-const BACKSTOP_TEST_CSS_OVERRIDE = 'html {background-image: none;}';
+/**
+ * OVERRIDE CSS
+ * Apply this CSS to the loaded page, as a way to override styles.
+ *
+ * Use this in an onReady script E.G.
+  ```
+  module.exports = async function(page, scenario) {
+    await require('./overrideCSS')(page, scenario);
+  }
+  ```
+ *
+ */
+
+  const BACKSTOP_TEST_CSS_OVERRIDE = `
+  html {
+    background-image: none;
+  }
+`;
 
 module.exports = async (page, scenario) => {
   // inject arbitrary css to override styles
-  await page.evaluate(`window._styleData = '${BACKSTOP_TEST_CSS_OVERRIDE}'`);
-  await page.evaluate(() => {
-    const style = document.createElement('style');
-    style.type = 'text/css';
-    const styleNode = document.createTextNode(window._styleData);
-    style.appendChild(styleNode);
-    document.head.appendChild(style);
+  await page.addStyleTag({
+    content: BACKSTOP_TEST_CSS_OVERRIDE
   });
 
   console.log('BACKSTOP_TEST_CSS_OVERRIDE injected for: ' + scenario.label);
