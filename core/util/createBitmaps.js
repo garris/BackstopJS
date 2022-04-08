ecae2bfae648efa42f80485b356444260e639453
Garris Shipon <garris@me.com>
2022-04-08 12:00:21 -0700

    update engine detect flow

--- core/util/createBitmaps.js
+++ core/util/createBitmaps.js
@@ -131,7 +131,7 @@ function delegateScenarios (config) {
 
   if (config.engine.startsWith('puppet')) {
     return pMap(scenarioViews, runPuppet, { concurrency: asyncCaptureLimit });
-  } else if (config.engine.startsWith('playwr')) {
+  } else if (config.engine.startsWith('play')) {
     return pMap(scenarioViews, runPlaywright, { concurrency: asyncCaptureLimit });
   } else if (/chrom./i.test(config.engine)) {
     logger.error('Chromy is no longer supported in version 5+. Please use version 4.x.x for chromy support.');
