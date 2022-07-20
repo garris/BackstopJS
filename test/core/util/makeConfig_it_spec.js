ece2736370dc8904201f24d51f6b0a7c5f5ad8c5
Andrei Bintintan <andy@interpid.eu>
2022-07-20 13:39:19 +0200

    Fix unit-test

--- test/core/util/makeConfig_it_spec.js
+++ test/core/util/makeConfig_it_spec.js
@@ -1,51 +1,55 @@
-const assert = require('assert');
-const path = require('path');
+const assert = require("assert");
+const path = require("path");
 
 process.chdir(__dirname);
 
-const makeConfig = require('../../../core/util/makeConfig');
-const { version } = require('../../../package');
-const configFile = require('./backstop');
+const makeConfig = require("../../../core/util/makeConfig");
+const { version } = require("../../../package");
+const configFile = require("./backstop");
 
 // root of backstop dir, not related to cwd
-const backstopDir = path.resolve(__dirname, '../../..');
+const backstopDir = path.resolve(__dirname, "../../..");
 
 const expectedConfig = {
   args: {},
   asyncCompareLimit: undefined,
   backstop: backstopDir,
   backstopVersion: version,
-  bitmaps_reference: path.resolve('backstop_data/bitmaps_reference'),
-  bitmaps_test: path.resolve('backstop_data/bitmaps_test'),
-  ci_report: path.resolve('backstop_data/ci_report'),
-  html_report: path.resolve('backstop_data/html_report'),
+  bitmaps_reference: path.resolve("backstop_data/bitmaps_reference"),
+  bitmaps_test: path.resolve("backstop_data/bitmaps_test"),
+  ci_report: path.resolve("backstop_data/ci_report"),
+  html_report: path.resolve("backstop_data/html_report"),
   openReport: true,
-  comparePath: path.resolve(backstopDir, 'compare/output'),
-  captureConfigFileNameDefault: path.resolve(backstopDir, 'capture/config.default.json'),
+  comparePath: path.resolve(backstopDir, "compare/output"),
+  captureConfigFileNameDefault: path.resolve(
+    backstopDir,
+    "capture/config.default.json"
+  ),
   engine: null,
-  engine_scripts: path.resolve('backstop_data/engine_scripts'),
-  engine_scripts_default: path.resolve(backstopDir, 'capture/engine_scripts'),
+  engine_scripts: path.resolve("backstop_data/engine_scripts"),
+  engine_scripts_default: path.resolve(backstopDir, "capture/engine_scripts"),
   perf: {},
   id: configFile.id,
-  report: ['browser'],
+  report: ["browser"],
   ciReport: {
-    format: 'junit',
-    testReportFileName: 'xunit',
-    testSuiteName: 'BackstopJS'
+    format: "junit",
+    testReportFileName: "xunit",
+    testSuiteName: "BackstopJS"
   },
-  compareConfigFileName: path.resolve('backstop_data/html_report/config.js'),
-  compareReportURL: path.resolve('backstop_data/html_report/index.html'),
+  compareConfigFileName: path.resolve("backstop_data/html_report/config.js"),
+  compareReportURL: path.resolve("backstop_data/html_report/index.html"),
   defaultMisMatchThreshold: 0.1,
   debug: false,
   resembleOutputOptions: undefined,
+  scenarioLogsInReports: undefined,
   dockerCommandTemplate: undefined,
-  archivePath: path.resolve('backstop_data/reports'),
+  archivePath: path.resolve("backstop_data/reports"),
   archiveReport: false
 };
 
-describe('make config it', function () {
-  it('should return the default config correctly', function () {
-    const actualConfig = makeConfig('test');
+describe("make config it", function() {
+  it("should return the default config correctly", function() {
+    const actualConfig = makeConfig("test");
 
     assert(actualConfig.tempCompareConfigFileName);
     delete actualConfig.tempCompareConfigFileName;
