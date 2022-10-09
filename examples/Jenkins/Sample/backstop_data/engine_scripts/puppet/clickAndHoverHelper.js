1018f68bbfa27afc9469c3003af09cc9398f138e
Florian Sellmayr <flosell@users.noreply.github.com>
2022-10-09 22:04:32 +0700

    Remove usage of deprecated/removed waitFor in clickAndHoverHelper scripts (fixes #1431) (#1441)

--- examples/Jenkins/Sample/backstop_data/engine_scripts/puppet/clickAndHoverHelper.js
+++ examples/Jenkins/Sample/backstop_data/engine_scripts/puppet/clickAndHoverHelper.js
@@ -4,16 +4,16 @@ module.exports = async (page, scenario) => {
   const postInteractionWait = scenario.postInteractionWait; // selector [str] | ms [int]
 
   if (hoverSelector) {
-    await page.waitFor(hoverSelector);
+    await page.waitForSelector(hoverSelector);
     await page.hover(hoverSelector);
   }
 
   if (clickSelector) {
-    await page.waitFor(clickSelector);
+    await page.waitForSelector(clickSelector);
     await page.click(clickSelector);
   }
 
   if (postInteractionWait) {
-    await page.waitFor(postInteractionWait);
+    await page.waitForTimeout(postInteractionWait);
   }
 };
