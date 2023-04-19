fde03f4acbf9eed2977b28693e8798f72a828d0f
Torsten Krah <krah.tm@gmail.com>
2023-04-19 13:25:15 +0200

    Rollback docker user change
    - defaults to root now again
    - added a recommendation to the README.md to configure a more
      appropriate user to run the test by changing the
      dockerCommandTemplate config option
    - fixes (#1466)

--- core/util/runDocker.js
+++ core/util/runDocker.js
@@ -2,7 +2,7 @@ const { spawn } = require('child_process');
 const version = require('../../package').version;
 const fs = require('./fs');
 
-const DEFAULT_DOCKER_COMMAND_TEMPLATE = 'docker run --rm -it --user $(id -u):$(id -g) --mount type=bind,source="{cwd}",target=/src backstopjs/backstopjs:{version} {backstopCommand} {args}';
+const DEFAULT_DOCKER_COMMAND_TEMPLATE = 'docker run --rm -it --mount type=bind,source="{cwd}",target=/src backstopjs/backstopjs:{version} {backstopCommand} {args}';
 
 module.exports.shouldRunDocker = (config) => config.args.docker;
 
