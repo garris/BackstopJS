97e171b1c3efb782adac569433e70314a7e9b58b
Dan Grebb <dan@dgrebb.com>
2023-08-01 19:18:56 -0400

    feat(report): adds test button, animates button while testing, then reloads upon completion

--- compare/output/run_tests_button.js
+++ compare/output/run_tests_button.js
@@ -5,15 +5,29 @@ function addTestButton() {
   const buttonText = document.createTextNode('Test');
   const settingsHeader = document.querySelector('section.header > div > section > div:nth-of-type(3)');
   testButton.id = 'dg--test-button';
-  testButton.class = 'darkreader';
+  testButton.classList.add('darkreader');
   testButton.appendChild(buttonText);
-  testButton.addEventListener("click", function() {
-    fetch('http://localhost:3000/test', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'multipart/form-data'
+  testButton.addEventListener("click", async function() {
+    try {
+      testButton.disabled = true;
+      testButton.classList.toggle("running", true);
+      const response = await fetch('http://localhost:3000/test', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      if (!response.ok) {
+        alert("The remote server doesn't appear to be running.");
+        throw new Error("BackstopJS Remote Server Not Running");
+      } else {
+        testButton.disabled = false;
+        testButton.classList.toggle("running", false);
+        location.reload();
       }
-    });
+    } catch (error) {
+      console.error("There was a problem requesting a test run from Backstop Remote.", error);
+    }
   });
 
   settingsHeader.prepend(testButton);
