87dc7d6b863ff5e5167a89a933f5890383774807
Dan Grebb <dan@dgrebb.com>
2023-10-17 17:52:05 -0400

    fix(remote): hard-refreshes the page on test error rather than window.alert

--- compare/output/extraButtons.js
+++ compare/output/extraButtons.js
@@ -1,11 +1,16 @@
-const filterStorageKey = 'backstopFilterValue';
-const settingsHeader = document.querySelector('section.header > div > section > div:nth-of-type(3)');
-const filterInput = document.getElementById('dg--filter-input');
+const filterStorageKey = "backstopFilterValue";
+const settingsHeader = document.querySelector(
+  "section.header > div > section > div:nth-of-type(3)"
+);
+const filterInput = document.getElementById("dg--filter-input");
 
 const setFilter = () => {
   const filterValue = getFilterStorage();
-  const ev = new Event('input', { bubbles: true });
-  const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+  const ev = new Event("input", { bubbles: true });
+  const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+    window.HTMLInputElement.prototype,
+    "value"
+  ).set;
   ev.simulated = true;
   if (filterValue) {
     nativeInputValueSetter.call(filterInput, filterValue);
@@ -28,100 +33,108 @@ const getFilterStorage = () => {
   return filterValue || false;
 };
 
-function addTestButton () {
-  const testButton = document.createElement('button');
-  const buttonText = document.createTextNode('Test');
-  testButton.id = 'dg--test-button';
-  testButton.classList.add('darkreader');
-  testButton.classList.add('dg--button');
+function addTestButton() {
+  const testButton = document.createElement("button");
+  const buttonText = document.createTextNode("Test");
+  testButton.id = "dg--test-button";
+  testButton.classList.add("darkreader");
+  testButton.classList.add("dg--button");
   testButton.appendChild(buttonText);
-  testButton.addEventListener('click', async function () {
+  testButton.addEventListener("click", async function () {
     const filter = getFilter();
-    const URL = `http://localhost:3000/test${filter && '?filter=' + filter}`;
+    const URL = `http://localhost:3000/test${filter && "?filter=" + filter}`;
     setFilterStorage(filter);
     try {
       testButton.disabled = true;
-      testButton.classList.toggle('running', true);
+      testButton.classList.toggle("running", true);
       const response = await fetch(URL, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'multipart/form-data'
-        }
+          "Content-Type": "multipart/form-data",
+        },
       });
       if (!response.ok) {
-        window.alert("The remote server doesn't appear to be running.");
-        throw new Error('BackstopJS Remote Server Not Running');
+        window.alert("The remote server no appear to be running.");
+        throw new Error("BackstopJS Remote Server Not Running");
       } else {
         testButton.disabled = false;
-        testButton.classList.toggle('running', false);
+        testButton.classList.toggle("running", false);
         window.location.reload();
       }
     } catch (error) {
-      testButton.classList.toggle('running', false);
-      window.alert("Backstop remote isn't running");
-      console.error('There was a problem requesting a test run from Backstop Remote.', error);
+      testButton.classList.toggle("running", false);
+      window.alert("Backstop remote no running");
+      console.error(
+        "There was a problem requesting a test run from Backstop Remote.",
+        error
+      );
     }
   });
 
   settingsHeader.prepend(testButton);
 }
 
-function addRefButton () {
-  const refButton = document.createElement('button');
-  const buttonText = document.createTextNode('Reference');
-  refButton.id = 'dg--ref-button';
-  refButton.classList.add('darkreader');
-  refButton.classList.add('dg--button');
+function addRefButton() {
+  const refButton = document.createElement("button");
+  const buttonText = document.createTextNode("Reference");
+  refButton.id = "dg--ref-button";
+  refButton.classList.add("darkreader");
+  refButton.classList.add("dg--button");
   refButton.appendChild(buttonText);
-  refButton.addEventListener('click', async function () {
+  refButton.addEventListener("click", async function () {
     const filter = getFilter();
     setFilterStorage(filter);
-    const URL = `http://localhost:3000/ref${filter && '?filter=' + filter}`;
+    const URL = `http://localhost:3000/ref${filter && "?filter=" + filter}`;
     try {
       refButton.disabled = true;
-      refButton.classList.toggle('running', true);
+      refButton.classList.toggle("running", true);
       const response = await fetch(URL, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'multipart/form-data'
-        }
+          "Content-Type": "multipart/form-data",
+        },
       });
       if (!response.ok) {
-        window.alert("The remote server doesn't appear to be running.");
-        throw new Error('BackstopJS Remote Server Not Running');
+        window.alert("The remote server no appear to be running.");
+        throw new Error("BackstopJS Remote Server Not Running");
       } else {
         refButton.disabled = false;
-        refButton.classList.toggle('running', false);
-        window.location.reload();
+        refButton.classList.toggle("running", false);
+        window.location.reload(true);
       }
     } catch (error) {
-      refButton.disabled = true;
-      refButton.classList.toggle('running', false);
-      window.alert("Backstop remote isn't running");
-      console.error('There was a problem requesting a reference run from Backstop Remote.', error);
+      refButton.classList.toggle("running", false);
+      window.location.reload(true);
+      console.error(
+        "There was a problem requesting a reference run from Backstop Remote.",
+        error
+      );
     }
   });
 
   settingsHeader.prepend(refButton);
 }
 
-async function pingUrl (url) {
+async function pingUrl(url) {
   try {
     await fetch(url, {
-      method: 'GET',
-      mode: 'no-cors',
-      cache: 'no-cache',
-      referrerPolicy: 'no-referrer'
+      method: "GET",
+      mode: "no-cors",
+      cache: "no-cache",
+      referrerPolicy: "no-referrer",
     }).then(() => {
       addTestButton();
       addRefButton();
     });
   } catch (err) {
-    console.log("%c The Backstop remote server isn't running! ", 'background: #222; color: #bada55');
+    console.log(
+      "%c The Backstop remote server no running! ",
+      "background: #222; color: #bada55"
+    );
     console.log(err);
   }
-  return 'error';
+  return "error";
 }
 
 document.body.onload = setFilter();
-document.body.onload = pingUrl('http://localhost:3000');
+document.body.onload = pingUrl("http://localhost:3000");
