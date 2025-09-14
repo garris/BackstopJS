9673c45672e59c252b6c76bd4b84f6fca3136b28
BenceSzalai <bence@sbnc.eu>
2025-09-14 14:04:07 +0200

    style: revert accidental whitespace changes

--- core/command/remote.js
+++ core/command/remote.js
@@ -6,40 +6,40 @@ const ssws = require.resolve('super-simple-web-server');
 
 module.exports = {
   execute: function (config) {
-	const MIDDLEWARE_PATH = path.resolve(config.backstop, 'remote');
-	const projectPath = path.resolve(config.projectPath);
+    const MIDDLEWARE_PATH = path.resolve(config.backstop, 'remote');
+    const projectPath = path.resolve(config.projectPath);
 
-	return new Promise(function (resolve, reject) {
-	  const port = getRemotePort();
-	  const args = [
-		ssws,
-		projectPath,
-		MIDDLEWARE_PATH,
-		`--config=${config.backstopConfigFileName}`
-	  ];
-	  const env = { SSWS_HTTP_PORT: port };
+    return new Promise(function (resolve, reject) {
+      const port = getRemotePort();
+      const args = [
+        ssws,
+        projectPath,
+        MIDDLEWARE_PATH,
+        `--config=${config.backstopConfigFileName}`
+      ];
+      const env = { SSWS_HTTP_PORT: port };
 
-	  logger.log(
-		`Starting remote with: ${process.execPath} ${args.join(' ')} with env ${JSON.stringify(env)}`
-	  );
+      logger.log(
+        `Starting remote with: ${process.execPath} ${args.join(' ')} with env ${JSON.stringify(env)}`
+      );
 
-	  const child = spawn(process.execPath, args, {
-		env: { ...process.env, ...env },
-		stdio: ['ignore', 'pipe', 'pipe']
-	  });
+      const child = spawn(process.execPath, args, {
+        env: { ...process.env, ...env },
+        stdio: ['ignore', 'pipe', 'pipe']
+      });
 
-	  child.on('error', (error) => {
-		if (error) {
-		  logger.log('Error running backstop remote:', error);
-		}
-	  });
+      child.on('error', (error) => {
+        if (error) {
+          logger.log('Error running backstop remote:', error);
+        }
+      });
 
-	  child.stdout.on('data', logger.log);
+      child.stdout.on('data', logger.log);
 
-	  child.stdout.on('close', data => {
-		logger.log('Backstop remote connection closed.', data);
-		resolve(data);
-	  });
-	});
+      child.stdout.on('close', data => {
+        logger.log('Backstop remote connection closed.', data);
+        resolve(data);
+      });
+    });
   }
 };
