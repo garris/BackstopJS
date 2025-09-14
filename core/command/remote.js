64224b6bf64f1ac68d9424a598c462e63c11b1a9
BenceSzalai <bence@sbnc.eu>
2025-09-14 13:59:25 +0200

    fix: remote failed with space in project path

--- core/command/remote.js
+++ core/command/remote.js
@@ -1,33 +1,45 @@
 const logger = require('../util/logger')('remote');
 const path = require('path');
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 const getRemotePort = require('../util/getRemotePort');
 const ssws = require.resolve('super-simple-web-server');
 
 module.exports = {
   execute: function (config) {
-    const MIDDLEWARE_PATH = path.resolve(config.backstop, 'remote');
-    const projectPath = path.resolve(config.projectPath);
+	const MIDDLEWARE_PATH = path.resolve(config.backstop, 'remote');
+	const projectPath = path.resolve(config.projectPath);
 
-    return new Promise(function (resolve, reject) {
-      const port = getRemotePort();
-      const commandStr = `node ${ssws} ${projectPath} ${MIDDLEWARE_PATH} --config=${config.backstopConfigFileName}`;
-      const env = { SSWS_HTTP_PORT: port };
+	return new Promise(function (resolve, reject) {
+	  const port = getRemotePort();
+	  const args = [
+		ssws,
+		projectPath,
+		MIDDLEWARE_PATH,
+		`--config=${config.backstopConfigFileName}`
+	  ];
+	  const env = { SSWS_HTTP_PORT: port };
 
-      logger.log(`Starting remote with: ${commandStr} with env ${JSON.stringify(env)}`);
+	  logger.log(
+		`Starting remote with: ${process.execPath} ${args.join(' ')} with env ${JSON.stringify(env)}`
+	  );
 
-      const child = exec(commandStr, { env: { ...env, PATH: process.env.PATH } }, (error) => {
-        if (error) {
-          logger.log('Error running backstop remote:', error);
-        }
-      });
+	  const child = spawn(process.execPath, args, {
+		env: { ...process.env, ...env },
+		stdio: ['ignore', 'pipe', 'pipe']
+	  });
 
-      child.stdout.on('data', logger.log);
+	  child.on('error', (error) => {
+		if (error) {
+		  logger.log('Error running backstop remote:', error);
+		}
+	  });
 
-      child.stdout.on('close', data => {
-        logger.log('Backstop remote connection closed.', data);
-        resolve(data);
-      });
-    });
+	  child.stdout.on('data', logger.log);
+
+	  child.stdout.on('close', data => {
+		logger.log('Backstop remote connection closed.', data);
+		resolve(data);
+	  });
+	});
   }
 };
